7142ef416757743c50246e6accb9239f0dcddcbd
ff-felipe <206090940+ff-felipe@users.noreply.github.com>
2025-04-29 14:02:25 -0300

    novo arquivo

---
+++ app02.js
@@ -0,0 +1,50 @@
+//**********************************primeiro ex ***************************************/
+
+// alert('Bem vindo(a)!')
+// const usuario = prompt('Digite seu acesso: ')
+// const validacao = (usuario === 'admin'||usuario === 'adm'||usuario === 'administrador')? alert('Login realizado, seja bem vindo!') : alert('Usuário inválido!')
+// console.log(validacao)
+
+//**********************************segundo ex ***************************************/
+// const suco = 'maça'
+// if(suco === 'laranja' || suco === 'abacaxi'){
+//     console.log('Fruta compatível com a receita')
+// } else{
+//     console.log('Fruta incompatível com a receita')
+// }
+
+
+//**********************************terceiro ex ***************************************/
+// const carrinhoVazio = false
+// const verificaCarrinho = carrinhoVazio? console.log('Não é possível continuar a compra, motivo: carrinho vazio.') : console.log('Prossiga com sua compra!')
+
+//**********************************quarto ex ***************************************/
+// const carrinhoVazio = true
+// if(carrinhoVazio !== false){
+//     console.log('Prossiga com sua compra!')
+// } else{
+//     console.log('Não é possível continuar a compra, motivo: carrinho vazio.')
+// }
+
+
+//**********************************quinto ex ***************************************/
+// const pontos = 230
+// const vidas = 10
+// const respostaPontos = (pontos >=50 && vidas >= 1)? console.log('Liberado, avance para o próximo nível!')  : console.log('Não pode avançar para o próximo nível.')
+
+
+
+//**********************************sexto ex ***************************************/
+// const pontos = 100
+// const vidas = 4
+// if(pontos >= 50 && vidas >= 1){
+//     console.log('Liberado, avance para o próximo nível!')
+// } else{
+//     console.log('Não pode avançar para o próximo nível.')
+// }
+
+
+//**********************************sétimo ex ***************************************/
+// bateria = 100
+// const result = (bateria <= 20)? console.log('Crítica') : (bateria < 80)? console.log('Moderada') : console.log('Cheia')
+
